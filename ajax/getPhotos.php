541bccc75bce7d0590a8401a147030e40935aade
Victor Fomenko <victorfomenko@me.com>
2014-05-17 17:32:02 +0400

    Change datebase password

--- ajax/getPhotos.php
+++ ajax/getPhotos.php
@@ -2,7 +2,7 @@
 	$url = $_REQUEST["url"];
 	$dbIP = 'localhost';
 	$dbUser = 'root';
-	$dbPass = 'infected29';
+	$dbPass = 'TwofRag83';
 	$dbName = 'victorfomenko';
 	$connection = mysql_connect ( $dbIP ,$dbUser, $dbPass ) or DIE( "Couldn't connect ot DB" );
 	mysql_select_db( $dbName ) 	or DIE( "Couldn't connect ot DB" );
