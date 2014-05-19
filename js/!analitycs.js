29c40a0dce338d1a70817b04203d47e373a915d3
Victor Fomenko <victorfomenko@me.com>
2014-05-19 16:37:36 +0400

    Add analytics

---
+++ js/!analitycs.js
@@ -0,0 +1,36 @@
+var _gaq = _gaq || [];
+_gaq.push(
+['_setAccount', 'UA-28757035-1'],
+['_setDomainName', 'victorfomenko.ru'],
+["_addOrganic", "mail.ru", "q"],
+["_addOrganic","rambler.ru", "words"],
+["_addOrganic","nigma.ru", "s"],
+["_addOrganic","blogs.yandex.ru", "text"],
+["_addOrganic","webalta.ru", "q"],
+["_addOrganic","aport.ru", "r"],
+["_addOrganic","akavita.by", "z"],
+["_addOrganic","meta.ua", "q"],
+["_addOrganic","bigmir.net", "q"],
+["_addOrganic","tut.by", "query"],
+["_addOrganic","all.by", "query"],
+["_addOrganic","i.ua", "q"],
+["_addOrganic","online.ua", "q"],
+["_addOrganic","a.ua", "s"],
+["_addOrganic","ukr.net", "search_query"],
+["_addOrganic","search.com.ua", "q"],
+["_addOrganic","search.ua", "query"],
+["_addOrganic","poisk.ru", "text"],
+["_addOrganic","km.ru", "sq"],
+["_addOrganic","liveinternet.ru", "ask"],
+["_addOrganic","gogo.ru", "q"],
+["_addOrganic","gde.ru", "keywords"],
+["_addOrganic","quintura.ru", "request"],
+['_trackPageview']
+);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.setAttribute('async', 'true');
+    document.documentElement.firstChild.appendChild(ga);
+    })();
