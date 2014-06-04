8f19ae66ed749747b041abc663a6eb35d017e418
Victor Fomenko <victorfomenko@me.com>
2014-06-04 18:25:54 +0400

    change auth controller

--- js/authController.js
+++ js/authController.js
@@ -2,13 +2,19 @@ function authController ($scope, $http) {
     $scope.auth = function() {
         var userName = $scope.userName;
         var userPass = $scope.userPass;
-        console.log(userName + "," + userPass);
-        $http.post('/ajax/auth.php', {
+        getAuthData('ajax/auth.php', {
             'auth_name': userName,
             'auth_pass': userPass
         })
-        .then(function(result) {
-            console.log(result.data);
+
+    }
+
+    var getAuthData = function(url, data){
+        $http.post(url, data)
+        .success(function(data,status){
+            console.log(data + status);
+        }).error(function(data, status){
+            console.log(status);
         });
     }
 }
