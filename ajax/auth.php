1d0413f875584efd9c92c48094203135b7a43552
Victor Fomenko <victorfomenko@me.com>
2014-06-04 22:40:56 +0400

    Get cockies. Login form 80%

--- ajax/auth.php
+++ ajax/auth.php
@@ -1,9 +1,13 @@
 <?
-	if(isset($_REQUEST['auth_name'])) {
+	$data = (json_decode(file_get_contents("php://input")));
+	$errors = array(
+		"login" => false,
+		"note" => ""
+	);
+	if(isset($data->auth_name)) {
 		require('dbConnect.php');
-
-		$userName = $_REQUEST['auth_name'];
-		$userPass = $_REQUEST['auth_pass'];
+		$userName = $data->auth_name;
+		$userPass = $data->auth_pass;
 
 		//Запрашиваем пароль пользователя из БД.
 		$q = "	SELECT id, name, password, last_login, admin
@@ -11,8 +15,19 @@
 			WHERE name='" . $userName . "' and password='" . $userPass . "'";
 
 		//Проверяем существование пользователя в БД.
-		if (!$user = getDataFromDB($q)) die("ERROR. Bad combination user and password");
-		if ($count = count($user) > 1) die("ERROR. Semething went wrong. Combinations of user and password > 1");
+		if (!$user = getDataFromDB($q)) {
+			$errors['login'] = false;
+			$errors['note'] =  "ERROR. Bad combination user and password";
+			header ("Content-Type: application/json");
+			print(json_encode($errors));
+			exit;
+		}
+		if ($count = count($user) > 1){
+			$errors['note'] =  "ERROR. Semething went wrong. Combinations of user and password > 1";
+			header ("Content-Type: application/json");
+			print(json_encode($errors));
+			exit;
+		};
 
 		//Создаём сессию, если авторизация прошла успешно.
 		session_start();
@@ -20,8 +35,12 @@
 		$_SESSION['user_name'] = $user[0]['name'];
 		$_SESSION['isadmin'] = $user[0]['admin'];
 		$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
-//		header("Location: http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-		print("Auth success!");
+
+		setcookie("userName", $_SESSION['user_name'], null, '/');
+		setcookie("isAdmin", $_SESSION['isadmin'], null, '/');
+
+		$errors['login'] = true;
+		echo json_encode($errors);
 		exit;
 	}
 
@@ -35,16 +54,5 @@
 		exit;
 	}
 	if (isset($_SESSION['user_id']) AND $_SESSION['ip'] == $_SERVER['REMOTE_ADDR']) return;
-
-	if (false) {
-		session_start();
-		$userData = array(
-			"id" => $_SESSION['user_id'],
-			"name" => $_SESSION['user_name'],
-			"isAdmin" => $_SESSION['isadmin']
-		);
-		header ("Content-Type: application/json");
-		echo(json_encode($userData));
-	}
 	exit;
 ?>
--- js/authController.js
+++ js/authController.js
@@ -2,19 +2,34 @@ function authController ($scope, $http) {
     $scope.auth = function() {
         var userName = $scope.userName;
         var userPass = $scope.userPass;
-        getAuthData('ajax/auth.php', {
+        var sendingData = {
             'auth_name': userName,
             'auth_pass': userPass
-        })
+        };
+        $http.post('/ajax/auth.php', sendingData)
+            .success(function(data){
+                $scope.isLogin = getCookie("PHPSESSID");
+                $scope.userName = getCookie("userName");
+                $scope.isAdmin = getCookie("isAdmin");
 
-    }
+                if(!data.login) {
+                    $scope.isFormError = true;
+                    return;
+                }
+                $scope.hideLoginForm();
+            })
+            .error(function(){
+                console.log("Something went wrong. AJAX ERROR.");
+            });
 
-    var getAuthData = function(url, data){
-        $http.post(url, data)
-        .success(function(data,status){
-            console.log(data + status);
-        }).error(function(data, status){
-            console.log(status);
-        });
+    };
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i].trim();
+            if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+        }
+        return "";
     }
 }
