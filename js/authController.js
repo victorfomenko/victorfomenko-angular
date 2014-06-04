f45a696e5113939c28def525f15ee34d915556a1
Victor Fomenko <victorfomenko@me.com>
2014-06-04 14:47:44 +0400

    start create js ajax request

--- js/authController.js
+++ js/authController.js
@@ -1,6 +1,14 @@
-function authController ($scope) {
+function authController ($scope, $http) {
     $scope.auth = function() {
         var userName = $scope.userName;
         var userPass = $scope.userPass;
+        console.log(userName + "," + userPass);
+        $http.post('/ajax/auth.php', {
+            'auth_name': userName,
+            'auth_pass': userPass
+        })
+        .then(function(result) {
+            console.log(result.data);
+        });
     }
 }
